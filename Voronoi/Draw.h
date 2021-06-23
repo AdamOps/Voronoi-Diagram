d4b3762cfb8260c59958345b45747e19b604d0f9
AdamOps <56359914+AdamOps@users.noreply.github.com>
2021-06-23 15:10:55 +0200

    Delete Draw.h

--- Voronoi/Draw.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <vector>
-
-#include "Shapes.h"
-#include "Parameters.h"
-
-void drawAll(sf::RenderWindow* window, parameters* settings, std::vector<site>* siteVector, sweepLine* line, beachLine* beach);
